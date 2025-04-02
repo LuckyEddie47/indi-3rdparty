cdb65bf2878004ad2e91d4ccba5e5581a4daa3e5
Ed Lee <ed@thefamilee.co.uk>
2025-04-02 13:57:33 +0100

    Initial checkin

---
+++ indi-onstep-aux/indi_onstep_aux.xml.cmake
@@ -0,0 +1,9 @@
+<?xml version="2.0" encoding="UTF-8"?>
+<driversList>
+<devGroup group="Auxiliary">
+    <device label="Onstep Aux" manufacturer="Onstep">
+        <driver name="Ontep Aux">indi_onstep_aux</driver>
+        <version>0.1</version>
+    </device>
+</devGroup>
+</driversList>
